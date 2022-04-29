b111c6e3539abcccd1bc3e641d99b2a84aaa9656
Ta Nhat <minhnhat3105@gmail.com>
2022-04-29 23:30:03 +0700

    deploy api to server, configuration connectionString

--- UniCEC.Data/Models/DB/UniCECContext.cs
+++ UniCEC.Data/Models/DB/UniCECContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -8,13 +8,15 @@ namespace UniCEC.Data.Models.DB
 {
     public partial class UniCECContext : DbContext
     {
+        private readonly IConfiguration _configuration;
         public UniCECContext()
         {
         }
 
-        public UniCECContext(DbContextOptions<UniCECContext> options)
+        public UniCECContext(DbContextOptions<UniCECContext> options, IConfiguration configuration)
             : base(options)
         {
+            _configuration = configuration;
         }
 
         public virtual DbSet<Blog> Blogs { get; set; }
@@ -51,7 +53,7 @@ namespace UniCEC.Data.Models.DB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Database=UniCEC;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("UniCEC"));
                 //
                 optionsBuilder.UseLazyLoadingProxies(); 
             }
