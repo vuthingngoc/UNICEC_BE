45d3127fe6377a4e01c75e9a525d7817aefa9b4f
NguyenTienAnh <anhnguyentien3006@gmail.com>
2022-05-23 20:42:57 +0700

    scaffold db

--- UniCEC.Data/Models/DB/Team.cs
+++ UniCEC.Data/Models/DB/Team.cs
@@ -14,13 +14,15 @@ namespace UniCEC.Data.Models.DB
 
         public int Id { get; set; }
         public int CompetitionId { get; set; }
+        public int TeamRoleId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int NumberOfStudentInTeam { get; set; }
         public string InvitedCode { get; set; }
-        public bool Status { get; set; }
+        public int Status { get; set; }
 
         public virtual Competition Competition { get; set; }
+        public virtual TeamRole TeamRole { get; set; }
         public virtual ICollection<ParticipantInTeam> ParticipantInTeams { get; set; }
     }
 }
