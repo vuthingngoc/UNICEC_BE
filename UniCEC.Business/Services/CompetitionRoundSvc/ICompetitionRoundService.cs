f24a8907820bfcfde90893152246929ddce11c70
Evil Gwi <minhnhat3105@gmail.com>
2022-08-08 19:30:10 +0700

    allow guest to view competition round

--- UniCEC.Business/Services/CompetitionRoundSvc/ICompetitionRoundService.cs
+++ UniCEC.Business/Services/CompetitionRoundSvc/ICompetitionRoundService.cs
@@ -9,7 +9,7 @@ namespace UniCEC.Business.Services.CompetitionRoundSvc
     public interface ICompetitionRoundService
     {
         public Task<ViewCompetitionRound> GetById(string token, int id);
-        public Task<PagingResult<ViewCompetitionRound>> GetByConditions(string token, CompetitionRoundRequestModel request);
+        public Task<PagingResult<ViewCompetitionRound>> GetByConditions(CompetitionRoundRequestModel request);
         public Task<List<ViewCompetitionRound>> Insert(string token, List<CompetitionRoundInsertModel> models);
         public Task Update(string token, CompetitionRoundUpdateModel model);
         public Task Delete(string token, int id);
